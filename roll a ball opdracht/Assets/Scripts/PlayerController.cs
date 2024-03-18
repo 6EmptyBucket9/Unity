85c1281fa1925d7d334da60256db8872da55a421
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-03-18 15:08:42 +0100

    Roll a ball project afgerond
    Klaar!

--- roll a ball opdracht/Assets/Scripts/PlayerController.cs
+++ roll a ball opdracht/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
-    public float speed = 0; 
-    
+    public float speed = 0;
+
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+
     }
     void OnMove(InputValue movementValue)
     {
@@ -22,12 +31,34 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winTextObject.SetActive(true);
+            speed = 0;
+        }
+
+    }
     private void FixedUpdate()
     {
-       
-        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement);
-        rb.AddForce(movement * speed); 
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.SetActive(false);
+
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
     }
 }
 
