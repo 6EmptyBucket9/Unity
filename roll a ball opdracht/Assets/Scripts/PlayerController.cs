efddc0c553c8b95c0c7e866c5f370765c7654409
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-04-04 14:39:23 +0200

    Level 2 added
    Level 2 toegevoegd

--- roll a ball opdracht/Assets/Scripts/PlayerController.cs
+++ roll a ball opdracht/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,6 +16,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
 
     public bool gameOver = false;
+
+    public bool nextLevel = false;
+
+    bool running = true;
+    public float delay = 3;
+    float timer;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
@@ -37,12 +44,40 @@ public class PlayerController : MonoBehaviour
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
-        if ((count >= 9) && (gameOver == true))
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name != "Level2")
         {
-            winTextObject.SetActive(true);
-            speed = 0;
-        }
+            if ((count >= 9) && (gameOver == true))
+            {
+                winTextObject.SetActive(true);
+                StartCoroutine(ExampleCoroutine());
+            }
 
+        }
+        else
+        {
+            if ((count >= 5) && (gameOver == true))
+            {
+                winTextObject.SetActive(true);
+                speed = 0;
+
+            }
+        }
+        IEnumerator ExampleCoroutine()
+        {
+            yield return new WaitForSeconds(3);
+
+            SetNextLevel();
+        }
+    }
+
+
+    void SetNextLevel()
+    {
+        if (nextLevel == true)
+        {
+            SceneManager.LoadScene("Level2");
+        }
     }
     private void FixedUpdate()
     {
@@ -60,14 +95,17 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
+
         }
         if (other.gameObject.CompareTag("PickUp GameOver"))
         {
             other.gameObject.SetActive(false);
             count++;
+            nextLevel = true;
             gameOver = true;
             SetCountText();
         }
+
     }
 }
 
