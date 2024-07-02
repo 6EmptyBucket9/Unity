143ca0de295e2f439aa85295595aee0fa85c9145
6EmptyBucket9 <jarno.ewijk@gmail.com>
2024-07-02 11:18:18 +0200

    commit

--- roll a ball opdracht/Assets/Scripts/PlayerController.cs
+++ roll a ball opdracht/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,8 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private int count;
+
+    public float timeRemaining; 
     private float movementX;
     private float movementY;
 
@@ -19,21 +21,39 @@ public class PlayerController : MonoBehaviour
 
     public bool nextLevel = false;
 
-    bool running = true;
+    bool timerIsRunning = false;
     public float delay = 3;
-    float timer;
+
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI timeText;
+
+    public TextMeshProUGUI loseText;
     public GameObject winTextObject;
+
+    public GameObject loseTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText();
+        timerIsRunning = true;
         winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
 
+        timeRemaining = 45;
+
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (scene.name == "Level2")
+        {
+            SetTimeText(timeRemaining);
+        }
+
+        // Debug log to check initial timeRemaining
+        Debug.Log("Start: Initial timeRemaining set to: " + timeRemaining);
     }
+
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -43,49 +63,105 @@ public class PlayerController : MonoBehaviour
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
         Scene scene = SceneManager.GetActiveScene();
-        if (scene.name != "Level2")
+        countText.text = "Points: " + count.ToString();
+        if (scene.name == "Level1")
         {
             if ((count >= 9) && (gameOver == true))
             {
                 winTextObject.SetActive(true);
                 StartCoroutine(ExampleCoroutine());
             }
-
         }
-        else
+        else if (scene.name == "Level2")
         {
             if ((count >= 5) && (gameOver == true))
             {
                 winTextObject.SetActive(true);
-                speed = 0;
-
+                StartCoroutine(ExampleCoroutine());
             }
         }
-        IEnumerator ExampleCoroutine()
+        else
         {
-            yield return new WaitForSeconds(3);
-
-            SetNextLevel();
+            if ((count >= 6) && (gameOver == true))
+            {
+                winTextObject.SetActive(true);
+                timeRemaining = 0;
+                speed = 0;
+            }
         }
     }
 
+    IEnumerator ExampleCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        SetNextLevel();
+    }
+
+    void SetTimeText(float timeToDisplay)
+    {
+        if (timeText != null)
+        {
+            if (timeRemaining > 0)
+            {
+                timeToDisplay += 1;
+            }
+
+            //format the timer
+            float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+            float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+            timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
 
     void SetNextLevel()
     {
-        if (nextLevel == true)
+        if (nextLevel)
         {
-            SceneManager.LoadScene("Level2");
+            if (SceneManager.GetActiveScene().name != "Level2")
+            {
+                SceneManager.LoadScene("Level2");
+            }
+            else
+            {
+                SceneManager.LoadScene("Level3");
+            }
         }
     }
+
     private void FixedUpdate()
     {
-
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        rb.AddForce(movement);
         rb.AddForce(movement * speed);
+
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                SetTimeText(timeRemaining);
+            }
+            else if (timeRemaining > 0 && gameOver == true)
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                SetTimeText(timeRemaining);
+                gameOver = true;
+                speed = 0;
+                if (loseTextObject != null)
+                {
+                    loseTextObject.SetActive(true);
+                }
+                else //check if object is assigned for fix
+                {
+                    Debug.LogError("loseTextObject is not assigned in the Inspector");
+                }
+
+
+            }
+        }
     }
+
     void OnTriggerEnter(Collider other)
     {
         other.gameObject.SetActive(false);
@@ -95,7 +171,6 @@ public class PlayerController : MonoBehaviour
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
-
         }
         if (other.gameObject.CompareTag("PickUp GameOver"))
         {
@@ -105,7 +180,5 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             SetCountText();
         }
-
     }
 }
-
